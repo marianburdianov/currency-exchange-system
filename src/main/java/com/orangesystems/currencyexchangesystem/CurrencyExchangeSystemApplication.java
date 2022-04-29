00a73e8f06a17a0120ff2521fcf2bd0d6e8cdf79
marianburdianov <marian.burdianov@gmail.com>
2022-04-29 14:31:33 +0300

    first commit

---
+++ src/main/java/com/orangesystems/currencyexchangesystem/CurrencyExchangeSystemApplication.java
@@ -0,0 +1,13 @@
+package com.orangesystems.currencyexchangesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orangesystems/currencyexchangesystem/CurrencyExchangeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orangesystems.currencyexchangesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
